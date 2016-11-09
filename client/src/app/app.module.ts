cf972fc063940609471c88872c5976e4b7a4bad0
Brijkishor Kushwaha (Product Engineering Service) <bkushwah@wipro.com>
2016-11-08 21:36:26 -0600

    8Nov16

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 //import { FormsModule } from '@angular/forms';
 //import { HttpModule,Http } from '@angular/http';
 import {LoginModule} from './login/login.module';
+import {SiteHeader} from './site-header/site-header.component';
 
 import { AppComponent } from './app.component';
 //import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeader
     //LoginComponent
   ],
   imports: [
---
+++ client/src/app/site-header/site-header.component.ts
@@ -0,0 +1,9 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector:'bass-site-header',
+    templateUrl:'site-header.component.html'
+})
+export class SiteHeader {
+
+}
